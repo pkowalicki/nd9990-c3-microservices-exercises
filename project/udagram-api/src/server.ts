4003e4e69116f3fb83001b2e28688b4a42ab931a
Przemyslaw Kowalicki <przemekkowalicki@gmail.com>
2020-12-22 11:57:46 +0100

    dockerized modules, mono left

--- project/udagram-api/src/server.ts
+++ project/udagram-api/src/server.ts
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 import cors from 'cors';
 import express from 'express';
 import {sequelize} from './sequelize';
@@ -10,8 +12,8 @@ import {V0_FEED_MODELS, V0_USER_MODELS} from './controllers/v0/model.index';
 
 
 (async () => {
-  await sequelize.addModels(V0_FEED_MODELS);
-  await sequelize.addModels(V0_USER_MODELS);
+  sequelize.addModels(V0_FEED_MODELS);
+  sequelize.addModels(V0_USER_MODELS);
   await sequelize.sync();
 
   const app = express();
